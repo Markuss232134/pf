39b9cd5fe10a7e85a6fd880af6d0ee01a6cfe585
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-14 10:00:39 +0200

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+function me(){
+    location.href = "me.html";
+}
+function nak(){
+    location.href = "nak.html";
+}
+function darb(){
+    location.href = "darbi.html";
+}
+function back(){
+    location.href = "index.html";
+}
+function darb1(){
+    location.href = "darbi1.html";
+}
